4ee6f27deee539137e3b44a714947efb8d83647c
ethan-hyatt <hyatt.b.ethan@gmail.com>
2024-04-21 16:18:28 -0400

    containerized application

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://hyattbethan-8000.theiadockernext'
-                 '-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 '-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://hyattbethan-8000.theiadockernext'
-                        '-1-labs-prod-theiak8s-4-tor01.proxy.cognitive'
+                        '-0-labs-prod-theiak8s-4-tor01.proxy.cognitive'
                         'class.ai']
 
 REST_FRAMEWORK = {
